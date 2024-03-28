833f9db87dcfa13040b19c9b7af26e126e79333d
cornflower26 <ap666a@gmail.com>
2024-03-28 02:40:49 -0400

    Hallelujah

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 #include <pke/openfhe.h>
 
 
-namespace lbcrypto {
+
     int main() {
         std::cout << "Hello, World!" << std::endl;
         //DCRTPoly a = DCRTPoly();
 
-        CCParams<CryptoContextCKKSRNS> parameters;
+        lbcrypto::CCParams<lbcrypto::CryptoContextCKKSRNS> parameters;
         parameters.SetMultiplicativeDepth(5);
         parameters.SetScalingModSize(10);
         return 0;
     }
-}
+
