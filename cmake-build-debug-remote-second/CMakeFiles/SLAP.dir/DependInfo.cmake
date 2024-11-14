c90f6260e54dfbac43b00fd1b656f121242ec1d7
cornflower26 <ap666a@gmail.com>
2024-11-14 12:07:04 -0500

    what
    ???

---
+++ cmake-build-debug-remote-second/CMakeFiles/SLAP.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ajanusze/SLAP/PSA-base-scheme.cpp" "CMakeFiles/SLAP.dir/PSA-base-scheme.cpp.o" "gcc" "CMakeFiles/SLAP.dir/PSA-base-scheme.cpp.o.d"
+  "/home/ajanusze/SLAP/PSA-cryptocontext.cpp" "CMakeFiles/SLAP.dir/PSA-cryptocontext.cpp.o" "gcc" "CMakeFiles/SLAP.dir/PSA-cryptocontext.cpp.o.d"
+  "/home/ajanusze/SLAP/main.cpp" "CMakeFiles/SLAP.dir/main.cpp.o" "gcc" "CMakeFiles/SLAP.dir/main.cpp.o.d"
+  "/home/ajanusze/SLAP/slaprns-scheme.cpp" "CMakeFiles/SLAP.dir/slaprns-scheme.cpp.o" "gcc" "CMakeFiles/SLAP.dir/slaprns-scheme.cpp.o.d"
+  "/home/ajanusze/SLAP/utils.cpp" "CMakeFiles/SLAP.dir/utils.cpp.o" "gcc" "CMakeFiles/SLAP.dir/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug-remote/CMakeFiles/SLAP.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ajanusze/SLAP/PSA-base-scheme.cpp" "/home/ajanusze/SLAP/cmake-build-debug-remote/CMakeFiles/SLAP.dir/PSA-base-scheme.cpp.o"
+  "/home/ajanusze/SLAP/PSA-cryptocontext.cpp" "/home/ajanusze/SLAP/cmake-build-debug-remote/CMakeFiles/SLAP.dir/PSA-cryptocontext.cpp.o"
+  "/home/ajanusze/SLAP/main.cpp" "/home/ajanusze/SLAP/cmake-build-debug-remote/CMakeFiles/SLAP.dir/main.cpp.o"
+  "/home/ajanusze/SLAP/slaprns-scheme.cpp" "/home/ajanusze/SLAP/cmake-build-debug-remote/CMakeFiles/SLAP.dir/slaprns-scheme.cpp.o"
+  "/home/ajanusze/SLAP/utils.cpp" "/home/ajanusze/SLAP/cmake-build-debug-remote/CMakeFiles/SLAP.dir/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/openfhe"
+  "/usr/local/include/openfhe/core"
+  "/usr/local/include/openfhe/pke"
+  "/usr/local/include/openfhe/binfhe"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
